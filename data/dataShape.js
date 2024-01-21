c4a32ae29d04cca8236e26a45ab960f2b2071d63
AntCannon <anthonycannonier@gmail.com>
2024-01-21 17:35:08 -0500

    complete getDinosaursDescription

--- data/dataShape.js
+++ data/dataShape.js
@@ -1,6 +1,6 @@
 // dinosaurs.js
-dinosaurs: dinosaurs - array of objects
-  dinosaur: dinosaur[i] - object representing a dinosaur
+dinosaurs: dinosaurs - array of objects - {object[]}
+  dinosaur: dinosaur[i] - object representing a dinosaur - {}
     dinosaurId: dinosaur[i].dinosaurId - "YLtkN9R37" - {string}
     name: dinosaur[i].name - "Allosaurus" - {string}
     pronunciation: dinosaur[i].pronunciation - "AL-oh-sore-us" - {string}
@@ -8,13 +8,13 @@ dinosaurs: dinosaurs - array of objects
     diet: dinosaur[i].diet - "carnivorous" - {string}
     lengthInMeters: dinosaur[i].lengthInMeters - 12 - {number}
     period: dinosaur[i].period - "Late Jurassic" - {string}
-    mya: dinosaur[i].mya - [156, 144] - array of numbers
+    mya: dinosaur[i].mya - [156, 144] - array of numbers - {number[]}
       mya value: dinosaur[i].mya[j] - million years ago - 156 - {number}
     info: dinosaur[i].info - "Allosaurus was an apex predator in the Late Jurassic in North America." - {string}
 
 // rooms.js
-rooms: rooms - array of objects
-  room: rooms[i] - object representing a room
+rooms: rooms - array of objects - {object[]}
+  room: rooms[i] - object representing a room - {}
     roomId: rooms[i].roomId - "zwfsfPU5u" - {string}
     name: rooms[i].name - "Entrance Room" - {string}
     requiredTicketPermissions: rooms[i].requiredTicketPermissions - [] -  array of strings {string[]}
@@ -24,36 +24,36 @@ rooms: rooms - array of objects
       connecting rooms: rooms[i].connectsTo[j] - "A6QaYdyKra" - {string}
 
 // tickets.js
-tickets: tickets - object of objects representing ticket types
-  general: tickets.general - object representing a ticket type
+tickets: tickets - object of objects representing ticket types - {<object{}>}
+  general: tickets.general - object representing a ticket type - {}
     description: tickets.general.description - "General Admission" - {string}
     priceInCents: tickets.general.priceInCents - {} - object holding ticket prices for visitors
       child: tickets.general.priceInCents.child - 2000 - {number}
       adult: tickets.general.priceInCents.adult - 3000 - {number}
       senior: tickets.general.priceInCents.senior - 2500 - {number}
 
-  membership: tickets.membership - object representing a ticket type
+  membership: tickets.membership - object representing a ticket type - {}
     description: tickets.membership.description - "General Admission" - {string}
     priceInCents: tickets.membership.priceInCents - {} - object holding ticket prices for visitors
       child: tickets.membership.priceInCents.child - 2000 - {number}
       adult: tickets.membership.priceInCents.adult - 3000 - {number}
       senior: tickets.membership.priceInCents.senior - 2500 - {number}
 
-  extras: tickets.extras - object representing a ticket type
+  extras: tickets.extras - object representing a ticket type - {}
     description: tickets.extras.description - "General Admission" - {string}
     priceInCents: tickets.extras.priceInCents - {} - object holding ticket prices for visitors
       child: tickets.extras.priceInCents.child - 2000 - {number}
       adult: tickets.extras.priceInCents.adult - 3000 - {number}
       senior: tickets.extras.priceInCents.senior - 2500 - {number}
 
-  education: tickets.education - object representing a ticket type
+  education: tickets.education - object representing a ticket type - {}
     description: tickets.education.description - "General Admission" - {string}
     priceInCents: tickets.education.priceInCents - {} - object holding ticket prices for visitors
       child: tickets.education.priceInCents.child - 2000 - {number}
       adult: tickets.education.priceInCents.adult - 3000 - {number}
       senior: tickets.education.priceInCents.senior - 2500 - {number}
 
-  terrace: tickets.terrace - object representing a ticket type
+  terrace: tickets.terrace - object representing a ticket type - {}
     description: tickets.terrace.description - "General Admission" - {string}
     priceInCents: tickets.terrace.priceInCents - {} - object holding ticket prices for visitors
       child: tickets.terrace.priceInCents.child - 2000 - {number}
--- src/01-dinosaur-facts.js
+++ src/01-dinosaur-facts.js
@@ -36,7 +36,6 @@ function getLongestDinosaur(dinosaurs) {
     if (dino.lengthInMeters > maxLength){
       maxLength = dino.lengthInMeters;
       maxDino = dino.name;
-      // console.log(`dino name`, dino.name);
     }
   }
   const maxLengthFeet = convertMetersToFeet(maxLength);
@@ -65,7 +64,16 @@ console.log(getLongestDinosaur(exampleDinosaurData));
  *  getDinosaurDescription(dinosaurs, "incorrect-id");
  *  //> "A dinosaur with an ID of 'incorrect-id' cannot be found."
  */
-function getDinosaurDescription(dinosaurs, id) {}
+function getDinosaurDescription(dinosaurs, id) {
+  for (let dino of dinosaurs) {
+    if (dino.dinosaurId === id) {
+      return `${dino.name} (${dino.pronunciation})\n${dino.info} It lived in the ${dino.period} period, over ${dino.mya[dino.mya.length-1]} million years ago.`
+    }
+  }
+  return `A dinosaur with an ID of '${id}' cannot be found.`
+}
+
+console.log(getDinosaurDescription(exampleDinosaurData, "GKl035EYKN"));
 
 /**
  * getDinosaursAliveMya()
