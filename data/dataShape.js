d06c71917f5ace1270fdd3835bf6e728eb2cfab6
AntCannon <anthonycannonier@gmail.com>
2024-01-21 13:01:27 -0500

    complete data shape for tickets.js

--- data/dataShape.js
+++ data/dataShape.js
@@ -9,7 +9,7 @@ dinosaurs: dinosaurs - array of objects
     lengthInMeters: dinosaur[i].lengthInMeters - 12 - {number}
     period: dinosaur[i].period - "Late Jurassic" - {string}
     mya: dinosaur[i].mya - [156, 144] - array of numbers
-      dinosaur[i].mya[j] - million years ago - {number}
+      mya value: dinosaur[i].mya[j] - million years ago - 156 - {number}
     info: dinosaur[i].info - "Allosaurus was an apex predator in the Late Jurassic in North America." - {string}
 
 // rooms.js
@@ -19,4 +19,43 @@ rooms: rooms - array of objects
     name: rooms[i].name - "Entrance Room" - {string}
     requiredTicketPermissions: rooms[i].requiredTicketPermissions - [] -  array of strings {string[]}
     dinosaurs: rooms[i].dinosaurs - ["qk1bNQA9_n"]- array of strings dinosaurId
+      dinosaurs in room: rooms[i].dinosaurs[j] - "qk1bNQA9_n" - {string}
     connectsTo: rooms[i].connectsTo - ["A6QaYdyKra"] - array of strings roomId
+      connecting rooms: rooms[i].connectsTo[j] - "A6QaYdyKra" - {string}
+
+// tickets.js
+tickets: tickets - object of objects representing ticket types
+  general: tickets.general - object representing a ticket type
+    description: tickets.general.description - "General Admission" - {string}
+    priceInCents: tickets.general.priceInCents - {} - object holding ticket prices for visitors
+      child: tickets.general.priceInCents.child - 2000 - {number}
+      adult: tickets.general.priceInCents.adult - 3000 - {number}
+      senior: tickets.general.priceInCents.senior - 2500 - {number}
+
+  membership: tickets.membership - object representing a ticket type
+    description: tickets.membership.description - "General Admission" - {string}
+    priceInCents: tickets.membership.priceInCents - {} - object holding ticket prices for visitors
+      child: tickets.membership.priceInCents.child - 2000 - {number}
+      adult: tickets.membership.priceInCents.adult - 3000 - {number}
+      senior: tickets.membership.priceInCents.senior - 2500 - {number}
+
+  extras: tickets.extras - object representing a ticket type
+    description: tickets.extras.description - "General Admission" - {string}
+    priceInCents: tickets.extras.priceInCents - {} - object holding ticket prices for visitors
+      child: tickets.extras.priceInCents.child - 2000 - {number}
+      adult: tickets.extras.priceInCents.adult - 3000 - {number}
+      senior: tickets.extras.priceInCents.senior - 2500 - {number}
+
+  education: tickets.education - object representing a ticket type
+    description: tickets.education.description - "General Admission" - {string}
+    priceInCents: tickets.education.priceInCents - {} - object holding ticket prices for visitors
+      child: tickets.education.priceInCents.child - 2000 - {number}
+      adult: tickets.education.priceInCents.adult - 3000 - {number}
+      senior: tickets.education.priceInCents.senior - 2500 - {number}
+
+  terrace: tickets.terrace - object representing a ticket type
+    description: tickets.terrace.description - "General Admission" - {string}
+    priceInCents: tickets.terrace.priceInCents - {} - object holding ticket prices for visitors
+      child: tickets.terrace.priceInCents.child - 2000 - {number}
+      adult: tickets.terrace.priceInCents.adult - 3000 - {number}
+      senior: tickets.terrace.priceInCents.senior - 2500 - {number}
