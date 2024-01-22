a008c9e1887d2277937c24bef65d40f1a57f9b91
AntCannon <anthonycannonier@gmail.com>
2024-01-22 12:37:12 -0500

    add num of dinosaurs message

--- src/01-dinosaur-facts.js
+++ src/01-dinosaur-facts.js
@@ -106,8 +106,18 @@ function getDinosaursAliveMya(dinosaurs, mya, key = 'dinosaurId') {
 
 // Stretch goals
 
+// How many dinosaurs are in the museum?
 /** getNumber of Dinosaurs */
 
+function getNumberOfDinosaurs(dinosaurs) {
+  return dinosaurs.length
+}
+
+function printNumOfDinoMess() {
+  console.log(`The Museum has ${getNumberOfDinosaurs(exampleDinosaurData)} life size dinosaurs on exhibit!`);
+}
+
+printNumOfDinoMess();
 
 module.exports = {
   getLongestDinosaur,
