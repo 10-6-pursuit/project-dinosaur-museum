3c5ab6211d590c944336f8ed17b7714fbd597a10
AntCannon <anthonycannonier@gmail.com>
2024-01-23 12:52:01 -0500

    move stretch goals from 01-dinosaur-facts.js to strtch.js

---
+++ stretch/stretch.js
@@ -0,0 +1,14 @@
+// Stretch goals
+
+// How many dinosaurs are in the museum?
+/** getNumber of Dinosaurs */
+
+function getNumberOfDinosaurs(dinosaurs) {
+  return dinosaurs.length
+}
+
+function printNumOfDinoMess() {
+  console.log(`The Dinosaur Museum has ${getNumberOfDinosaurs(exampleDinosaurData)} life size dinosaurs on exhibit!`);
+}
+
+printNumOfDinoMess();
