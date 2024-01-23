58a420d42b586c978685531e0c9339c690ad250e
AntCannon <anthonycannonier@gmail.com>
2024-01-23 12:52:42 -0500

    move stretch goals from 01-dinosaur-facts.js to strtch.js

--- src/01-dinosaur-facts.js
+++ src/01-dinosaur-facts.js
@@ -103,22 +103,6 @@ function getDinosaursAliveMya(dinosaurs, mya, key = 'dinosaurId') {
   }
   return res;
 }
-
-// Stretch goals
-
-// How many dinosaurs are in the museum?
-/** getNumber of Dinosaurs */
-
-function getNumberOfDinosaurs(dinosaurs) {
-  return dinosaurs.length
-}
-
-function printNumOfDinoMess() {
-  console.log(`The Dinosaur Museum has ${getNumberOfDinosaurs(exampleDinosaurData)} life size dinosaurs on exhibit!`);
-}
-
-printNumOfDinoMess();
-
 module.exports = {
   getLongestDinosaur,
   getDinosaurDescription,
