a02b8c55e55ace5597727ab7eb37ee9bd3556129
Ari-Rosalie <collado.arr@gmail.com>
2024-01-23 19:53:31 -0500

    first and second problem complete

--- src/01-dinosaur-facts.js
+++ src/01-dinosaur-facts.js
@@ -22,7 +22,23 @@ const exampleDinosaurData = require("../data/dinosaurs");
  *  getLongestDinosaur(dinosaurs);
  *  //> { Brachiosaurus: 98.43 }
  */
-function getLongestDinosaur(dinosaurs) {}
+function getLongestDinosaur(dinosaurs) {
+  let longestLength = 0
+  let dinosaur = {}
+  let dinoName 
+  if(0 === dinosaurs.length)
+  return {}
+   for (let i = 0; i < dinosaurs.length; i++) {
+      if (dinosaurs[i].lengthInMeters > longestLength)  {
+        longestLength = dinosaurs[i].lengthInMeters
+         dinoName = dinosaurs[i].name
+       }
+  }
+  dinosaur[dinoName] = (longestLength * 3.281)
+  return dinosaur
+}
+
+
 
 /**
  * getDinosaurDescription()
@@ -44,7 +60,18 @@ function getLongestDinosaur(dinosaurs) {}
  *  getDinosaurDescription(dinosaurs, "incorrect-id");
  *  //> "A dinosaur with an ID of 'incorrect-id' cannot be found."
  */
-function getDinosaurDescription(dinosaurs, id) {}
+function getDinosaurDescription(dinosaurs, id) {
+  let response
+  for (let i = 0; i < dinosaurs.length; i++) {
+    if (dinosaurs[i].dinosaurId === id) {
+        response =  `${dinosaurs[i].name} (${dinosaurs[i].pronunciation})\n${dinosaurs[i].info} It lived in the ${dinosaurs[i].period} period, over ${dinosaurs[i].mya[dinosaurs[i].mya.length - 1]} million years ago.`
+        break
+    } else if (dinosaurs[i].dinosaurId !== id) {
+      response =  `A dinosaur with an ID of '${id}' cannot be found.`
+    }
+  }
+  return response
+}
 
 /**
  * getDinosaursAliveMya()
