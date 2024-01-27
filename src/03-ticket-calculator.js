79448b827b1e16b1250222a58247521fe4f57fe0
Bryan Alcantara <bryanalcantara@Bryans-MacBook-Air.local>
2024-01-27 15:49:37 -0500

    finished the project

--- src/03-ticket-calculator.js
+++ src/03-ticket-calculator.js
@@ -125,8 +125,32 @@ const exampleTicketData = require("../data/tickets");
     purchaseTickets(tickets, purchases);
     //> "Ticket type 'discount' cannot be found."
  */
-function purchaseTickets(ticketData, purchases) {}
-
+function upperCaseFirst(str){
+return str[0].toUpperCase() + str.slice(1)
+}
+function purchaseTickets(ticketData, purchases) {
+  let priceOfActivity;
+  let receipt = "Thank you for visiting the Dinosaur Museum!\n-------------------------------------------\n"
+  let sum = 0;
+  for(let i = 0;i < purchases.length;i++){
+    let {ticketType,entrantType,extras} = purchases[i];
+    if(typeof calculateTicketPrice(ticketData,purchases[i])==="string"){
+      return calculateTicketPrice(ticketData,purchases[i]);
+    }
+    if(purchases[i]["extras"].length>0){
+    priceOfActivity = calculateTicketPrice(ticketData,purchases[i])/100
+    sum += calculateTicketPrice(ticketData,purchases[i])/100;
+    receipt += `${upperCaseFirst(entrantType)} ${upperCaseFirst(ticketType)} Admission: $${priceOfActivity}.00 (${(extras[0]? `${upperCaseFirst(extras[0])} Access`:"")}${(extras[1]? `, ${upperCaseFirst(extras[1])} Access`:"")}${(extras[2]? `, ${upperCaseFirst(extras[2])} Access`:"")})\n`
+  }else{
+    priceOfActivity = calculateTicketPrice(ticketData,purchases[i])/100
+    sum += calculateTicketPrice(ticketData,purchases[i])/100;
+    receipt += `${upperCaseFirst(entrantType)} ${upperCaseFirst(ticketType)} Admission: $${priceOfActivity}.00\n`
+  }
+}
+  receipt +=  `-------------------------------------------
+TOTAL: $${sum}.00`
+  return receipt;
+}
 // Do not change anything below this line.
 module.exports = {
   calculateTicketPrice,
