ad15cb99504069ddd1234ac67d7e1b646bba7d42
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-01-26 19:07:35 -0500

    Function 5 completed

--- src/02-room-details.js
+++ src/02-room-details.js
@@ -68,7 +68,42 @@ function getRoomByDinosaurName(dinosaurs, rooms, dinosaurName) {
       "Kit Hopkins Education Wing"
     ]
  */
-function getConnectedRoomNamesById(rooms, id) {}
+function getConnectedRoomNamesById(rooms, id) {
+  let roomIds = [];
+  let result = [];
+
+  for (let index of rooms) {
+    if (index.roomId === id) {
+      roomIds = index.connectsTo;
+    }
+  }
+
+  if (!roomIds.length) return `Room with ID of '${id}' could not be found.`;
+
+
+  for (i of roomIds) {
+    switch (i) {
+      case "A6QaYdyKra": result.push('Ticket Center'); break;
+      case "zwfsfPU5u": result.push('Entrance Room'); break;
+      case "aIA6tevTne": result.push('Coat Check Room'); break;
+      case "dpQnu5wgaN": result.push('Ellis Family Hall'); break;
+      case "L72moIRcrX": result.push('Kit Hopkins Education Wing'); break;
+      case "Ys2Trg-1OT": result.push('Terrell Leon Lecture Room'); break;
+      case "0eNtkY5WoA": result.push('Haley Hall'); break;
+      case "dBZeK6vhpt": result.push('Paxton Decker Terrace'); break;
+      case "VEr3w2ca_v": result.push('Cabrera Hall'); break;
+      case "Y707HL8uP9": result.push('Roberts Room'); break;
+      case "Gp6nCN1JGT": result.push('Reyes Hall'); break;
+      case "1FMoeqQxFk": result.push('Blackwell Amphitheater'); break;
+      case "qi5e4IFDby": result.push('Bryan Decker Hall'); break;
+      case "nt85di9a1V": result.push('Owen Family Room'); break;
+    }
+  }
+  
+  if (!result.length) return `Room with ID of 'incorrect-id' could not be found.`;
+
+  return result;
+}
 
 module.exports = {
   getRoomByDinosaurName,
