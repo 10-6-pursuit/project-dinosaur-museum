2a12c8a8d3b2e4f9b6758b1a9c4e6ef3d6cd185d
ayoublos <abelibel@pursuit.org>
2024-01-27 17:48:06 -0800

    solved purchaseTickets function

--- src/03-ticket-calculator.js
+++ src/03-ticket-calculator.js
@@ -197,7 +197,7 @@ let str1=``;
     
     }
      if(typeof sum===`string`){return sum}
-     else return "Thank you for visiting the Dinosaur Museum!\n------------------------------------------"+str1+`\n-------------------------------------------\nTOTAL: $${sum/100}.00`
+     else return "Thank you for visiting the Dinosaur Museum!\n-------------------------------------------"+str1+`\n-------------------------------------------\nTOTAL: $${sum/100}.00`
     
    
 
